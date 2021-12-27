376dd2787d3df7cde546d298e7853281dd451881
Robert Neumann <robert.neumann01@gmail.com>
2021-12-27 22:51:10 +0100

    add ad and sort it

--- terraform/compute/data.tf
+++ terraform/compute/data.tf
@@ -1,11 +1,16 @@
+data "oci_identity_availability_domain" "ad_1" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
+
 data "oci_identity_availability_domain" "ad_2" {
   compartment_id = var.tenancy_ocid
   ad_number      = 2
 }
 
-data "oci_identity_availability_domain" "ad_1" {
+data "oci_identity_availability_domain" "ad_3" {
   compartment_id = var.tenancy_ocid
-  ad_number      = 1
+  ad_number      = 3
 }
 
 resource "random_string" "cluster_token" {
