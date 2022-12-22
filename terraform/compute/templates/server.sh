f352fd9a1d3f6605c123d339cecaedec2d6b65f5
Chris Hennings <hennings@gmail.com>
2022-12-21 17:26:14 -0800

    continued refactoring

--- terraform/compute/templates/server.sh
+++ terraform/compute/templates/server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [[ "$HOSTNAME" =~ "server-1" ]]; then
+if [[ "$HOSTNAME" =~ "server_0" ]]; then
     curl -sfL https://get.k3s.io | K3S_TOKEN=${token} sh -s - server --cluster-init
 
-elif [[ "$HOSTNAME" =~ "server-2" || "$HOSTNAME" =~ "server-3"]]; then
+else
     curl -sfL https://get.k3s.io | K3S_TOKEN=${token} sh -s - server --server https://${server_0_ip}:6443
-
-
