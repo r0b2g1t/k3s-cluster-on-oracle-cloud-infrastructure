20fd571d229656916a4c4350472165b6cf58d226
Chris Hennings <hennings@gmail.com>
2022-12-22 13:41:33 -0800

    unforce error

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,14 @@ terraform {
     }
   }
 }
-/*
+
 provider "oci" {
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key = var.private_key
-}*/
+}
 
 module "network" {
   source = "./network"
