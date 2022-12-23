9921f893e966d36a7e58a9b4744e650fa7552cd9
Chris Hennings <hennings@gmail.com>
2022-12-22 20:25:47 -0800

    no private key password

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "oci" {
   region           = var.region
-  tenancy_ocid     = "test"
+  tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key      = var.private_key
-  private_key_password = var.private_key_password
+  #private_key_password = var.private_key_password
 }
 
 module "network" {
