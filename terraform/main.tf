1584a5fb1b94da516d9841504f90e84f971d27f7
Chris Hennings <hennings@gmail.com>
2022-12-22 21:00:04 -0800

    comment out all options

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "oci" {
-  region           = var.region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key      = var.private_key
+  #region           = var.region
+  #tenancy_ocid     = var.tenancy_ocid
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
+  #private_key      = var.private_key
   #private_key_password = var.private_key_password
 }
 
