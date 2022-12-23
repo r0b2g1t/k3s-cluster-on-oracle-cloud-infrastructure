7a0743ff430529d83a06c075b35a19516053b719
Chris Hennings <hennings@gmail.com>
2022-12-23 10:07:21 -0800

    variable provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "oci" {
-  #region           = var.region
-  #tenancy_ocid     = var.tenancy_ocid
-  #user_ocid        = var.user_ocid
-  #fingerprint      = var.fingerprint
-  #private_key      = var.private_key
-  #private_key_password = var.private_key_password
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key      = var.private_key
+  private_key_password = var.private_key_password
 }
 
 module "network" {
