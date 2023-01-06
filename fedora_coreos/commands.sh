767a26cc1eda473eb7f1ee1338f2ae74a3b5c908
m5lapp <git@m5l.app>
2023-01-06 00:58:31 +0000

    Corrected mistake in K3s installation commands

--- fedora_coreos/commands.sh
+++ fedora_coreos/commands.sh
@@ -59,12 +59,12 @@ sudo dnf install podman butane
 
 podman container run --interactive \
     --rm quay.io/coreos/butane:release \
-    --pretty --strict < fcos.bu > config.ign
+    --pretty --strict < k3s-agent-1.bu > k3s-agent-1.ign
 
 sudo wipefs -a /dev/sdb
 
 sudo podman run --pull=always --privileged --rm \
     -v /dev:/dev -v /run/udev:/run/udev -v .:/data -w /data \
     quay.io/coreos/coreos-installer:release \
-    install /dev/sdb --architecture x86_64 -i config.ign
+    install /dev/sdb --architecture x86_64 -i k3s-agent-1.ign
     # The --copy-network flag can be used to copy the network from the host.
