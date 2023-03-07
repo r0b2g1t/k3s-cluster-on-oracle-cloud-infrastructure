1835b8af4ca55e6bea1e26bc0063d33746a4e127
Robert Neumann <r0b2g1t@users.noreply.github.com>
2023-03-07 13:07:38 +0100

    cleanup iptables

--- terraform/compute/templates/server.sh
+++ terraform/compute/templates/server.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "Clearing iptables"
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+netfilter-persistent save
+
 if [[ "$HOSTNAME" =~ "server_0" ]]; then
     curl -sfL https://get.k3s.io | K3S_TOKEN=${token} sh -s - server --cluster-init
 
